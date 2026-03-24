baf602d37709a8aae91e7bc435570669d854235d
Ruchi Mulik <157717762+mulikruchi07@users.noreply.github.com>
2026-03-25 01:48:49 +0530

    Change extract_text to return list of detected text
    Refactor extract_text function to return a list of lines instead of a single string.

--- ocr_engine.py
+++ ocr_engine.py
@@ -1,16 +1,15 @@
 from paddleocr import PaddleOCR
 
-ocr = PaddleOCR(
-    use_angle_cls=True,
-    lang='en'
-)
+ocr = PaddleOCR(use_angle_cls=True, lang='en')
 
 def extract_text(image_path):
     result = ocr.ocr(image_path, cls=True)
 
-    text = ""
+    lines = []
+
     for line in result:
         for word in line:
-            text += word[1][0] + " "
-    
-    return text
+            text = word[1][0]
+            lines.append(text)
+
+    return lines   
