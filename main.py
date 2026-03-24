d18684d3002b30ff6f4930e50184a3d99246a6a5
Ruchi Mulik <157717762+mulikruchi07@users.noreply.github.com>
2026-03-25 01:48:15 +0530

    Update OCR to extract and print lines from image
    Modified OCR processing to handle lines instead of full text.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from matplotlib import lines
+
 from camera import capture_image
 from preprocess import preprocess_image
 from ocr_engine import extract_text
@@ -16,11 +18,14 @@ def main():
     cv2.imwrite("processed.jpg", processed)
 
     print("Running OCR...")
-    text = extract_text(img_path)
+    lines = extract_text(img_path)
+    lines = [l.replace(".", "").strip() for l in lines]
 
-    print("\nFull Text:\n", text)
+    print("\n--- OCR LINES ---")
+    for l in lines:
+        print(l)
 
-    data = extract_dynamic_fields(text)
+    data = extract_dynamic_fields(lines)
     if data is None:
         data = {}
     data = filter_important_fields(data)
